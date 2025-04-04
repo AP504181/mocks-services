69876ca4ba34521e450517b6b39fe1d8adebb053
Alfredo Pérez Mendoza <alfredo.perez1@profuturo.com.mx>
2025-04-04 14:49:22 -0600

    Update MsGestionTokenServiceApplicationTests.java

--- mock-api-services/src/test/java/com/profuturo/mock/services/MsGestionTokenServiceApplicationTests.java
+++ mock-api-services/src/test/java/com/profuturo/mock/services/MsGestionTokenServiceApplicationTests.java
@@ -2,7 +2,9 @@ package com.profuturo.mock.services;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootTest
 class MsGestionTokenServiceApplicationTests {
 
